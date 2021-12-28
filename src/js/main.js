1094fb81332ba175c6b5fb586b6db83c1796b1e6
Sandra Gutierrez <sandsan12@gmail.com>
2021-12-28 14:23:08 +0100

    Create dev branch and start js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
-
 console.log('>> Ready :)');
+
+// HTML elements
+const inputText = document.querySelector('.js-inputText');
+const btnSearch = document.querySelector('.js-btnSearch');
+const btnReset = document.querySelector('.js-btnReset');
+const listFav = document.querySelector('.js-listFav');
+const listRes = document.querySelector('.js-listRes');
+
+// Functions
+function handlerClickSearch(event){
+  event.preventDefault();
+  console.log('Funciona');
+}
+
+
+// Listeners
+btnSearch.addEventListener('click', handlerClickSearch);
+
