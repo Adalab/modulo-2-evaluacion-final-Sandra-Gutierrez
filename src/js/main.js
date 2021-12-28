5c720936d26ff8cf69a6f19ea6c7433ed4429d87
Sandra Gutierrez <sandsan12@gmail.com>
2021-12-28 17:59:46 +0100

    Add search function and render image + title

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,39 @@ const btnSearch = document.querySelector('.js-btnSearch');
 const btnReset = document.querySelector('.js-btnReset');
 const listFav = document.querySelector('.js-listFav');
 const listRes = document.querySelector('.js-listRes');
+let anime = '';
 
 // Functions
+function takeField(){
+  const nameAnime = inputText.value;
+  return nameAnime;
+}
+
+function getAnimeResults(){
+  listRes.innerHTML = '';
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${anime}`)
+    .then( (response) => response.json() )
+    .then( (data) => {
+      let arrResults = data.results;
+      console.log(arrResults);
+      // Renderizo en HTM la portada y el titulo
+      for(let i = 0; i < arrResults.length ; i++){
+        const imgAnime = data.results[i].image_url;
+        const titleAnime = data.results[i].title;
+        const itemList = `<li><img src="${imgAnime}"><p>${titleAnime}</p></li>`;
+        listRes.innerHTML += itemList;
+      }
+    });
+}
+
 function handlerClickSearch(event){
   event.preventDefault();
-  console.log('Funciona');
+  // Recojo el nombre de la serie
+  anime = takeField();
+  console.log(anime);
+
+  // Petición fetch a la API
+  getAnimeResults();
 }
 
 
