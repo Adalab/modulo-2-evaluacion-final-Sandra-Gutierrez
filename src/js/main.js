b531bcf7a2997995126f29c48baa6f5dbd0c55dd
Sandra Gutierrez <sandsan12@gmail.com>
2021-12-28 19:33:22 +0100

    Add save to favorites function and delete button

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ const btnSearch = document.querySelector('.js-btnSearch');
 const btnReset = document.querySelector('.js-btnReset');
 const listFav = document.querySelector('.js-listFav');
 const listRes = document.querySelector('.js-listRes');
+
 let anime = '';
 
 // Functions
@@ -21,13 +22,17 @@ function getAnimeResults(){
     .then( (response) => response.json() )
     .then( (data) => {
       let arrResults = data.results;
-      console.log(arrResults);
-      // Renderizo en HTM la portada y el titulo
+      // Renderizo en HTML la portada y el titulo
       for(let i = 0; i < arrResults.length ; i++){
         const imgAnime = data.results[i].image_url;
         const titleAnime = data.results[i].title;
-        const itemList = `<li><img src="${imgAnime}"><p>${titleAnime}</p></li>`;
+        const itemList = `<li class='js-itemList'><img src="${imgAnime}"><p>${titleAnime}</p></li>`;
         listRes.innerHTML += itemList;
+      }
+      // Añado evento click a los elementos de mi lista
+      const itemLi = document.querySelectorAll('.js-itemList');
+      for(let i = 0 ; i < itemLi.length ; i++){
+        itemLi[i].addEventListener('click', handlerClickFav);
       }
     });
 }
@@ -36,10 +41,28 @@ function handlerClickSearch(event){
   event.preventDefault();
   // Recojo el nombre de la serie
   anime = takeField();
-  console.log(anime);
-
-  // Petición fetch a la API
+  // Petición fetch a la API + Renderizar resultados
   getAnimeResults();
+}
+
+function renderFavs(fav){
+  // Clono el nodo fav
+  const itemFav = fav.cloneNode(true);
+  // Crear boton borrar
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'js-deleteBtn');
+  const contentBtn = document.createTextNode('Borrar');
+  deleteBtn.appendChild(contentBtn);
+  // Añadir boton borrar al item
+  itemFav.appendChild(deleteBtn);
+  // Añado el item clonado a mi lista de favoritos
+  listFav.appendChild(itemFav);
+}
+
+function handlerClickFav(event){
+  const animeFav = event.currentTarget;
+  // Renderizo el elemento clicado en favoritos
+  renderFavs(animeFav);
 }
 
 
